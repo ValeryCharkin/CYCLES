f19b488cda78c065cf2b1468c7ea5ce06049bda7
ValeryCharkin <charkin.valery@gmail.com>
2024-09-22 18:56:03 +0300

    change commit 1

--- src/test/java/ru/netology/javaqa/javamvn/services/test/VacantionCountTest.java
+++ src/test/java/ru/netology/javaqa/javamvn/services/test/VacantionCountTest.java
@@ -1,7 +1,7 @@
 package ru.netology.javaqa.javamvn.services.test;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -12,21 +12,21 @@ public class VacantionCountTest {
     //@Test
     @ParameterizedTest
 
-    @CsvFileSource (files = "src/test/resources/VacantionService.csv")
-    public void VacantionCount(int income, int expenses, int threshold,int expected) {
+    @CsvFileSource(files = "src/test/resources/VacantionService.csv")
+    public void VacantionCount(int income, int expenses, int threshold, int expected) {
         VacantionCount service = new VacantionCount();
-        int actual = service.calculate( income,  expenses,  threshold);
+        int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(actual, expected);
 
     }
 }
 
-    //@Test
-    //public void VacantionCount2() {
-    //VacantionCount service = new VacantionCount();
-        //int expected = 2;
-        //int actual = service.calculate(100000, 60000, 150000);
-        //Assertions.assertEquals(actual, expected);
+//@Test
+//public void VacantionCount2() {
+//VacantionCount service = new VacantionCount();
+//int expected = 2;
+//int actual = service.calculate(100000, 60000, 150000);
+//Assertions.assertEquals(actual, expected);
 //}
 //}
 
